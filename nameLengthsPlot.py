a48b0d17e371581f5fef00481325289531f871f0
Mike Overby <MikeOverby@outlook.com>
2015-03-26 12:06:13 -0700

    More generic, added tracking of all lengths
    Made the year range changeable from main()
    
    Now keeping track of a list of the list of all of the name lengths in
    the years, for use in a box-and-whisker plot.

--- nameLengthsPlot.py
+++ nameLengthsPlot.py
@@ -24,35 +24,50 @@ def readInYear(year):
 
     #Print lengths of first three names in year for test purposes
     #print(fLengths[0], fLengths[1], fLengths[2],":",mLengths[0], mLengths[1], mLengths[2],)
-    return average(fLengths), average(mLengths)
+    return fLengths, mLengths, average(fLengths), average(mLengths)
 
-def processData():
+def processData(startYear, endYear):
+    fYearlyLengths = []
+    mYearlyLengths = []
+
     femaleLen = []
     maleLen = []
+
     years = []
-    year = 1880
-    while year <= 2013:
+
+    year = startYear
+    while year <= endYear:
         years.append(year)
-        fAvg, mAvg = readInYear(year)
+        fem, male, fAvg, mAvg = readInYear(year)
+
+        fYearlyLengths.append(fem)
+        mYearlyLengths.append(male)
+        
         femaleLen.append(fAvg)
         maleLen.append(mAvg)
         #print(fAvg,mAvg)
         #print()
         
         year += 1
-    return femaleLen, maleLen, years
+    return fYearlyLengths, mYearlyLengths, femaleLen, maleLen, years
 
 def plotData(femaleLen, maleLen, years):
     plt.plot(years, femaleLen)
     plt.plot(years, maleLen)
-    plt.title("Name lengths in the United States (1880-2013)")
+    plt.legend(["Female", "Male"], loc=0)
+    plt.title("Name lengths in the United States ("
+                  + str(years[0]) + "-" + str(years[-1]) + ")")
     plt.ylabel("Name length (letters)")
     plt.xlabel("Year")
     plt.show()
     
 def main():
-    femaleLen, maleLen, years = processData()
+    start = 1880 #Possibly user input
+    end = 2013
+    fYearlyData, mYearlyData, femaleLen, maleLen, years = processData(start, end)
     plotData(femaleLen, maleLen, years)
+
+    print(fYearlyData[0])
     
     #The average overall lengths of both sexes' names
     #print(average(femaleLen))
